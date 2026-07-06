ee22df58d9328079353bcd47e3adab39f3b661e6
Alnit112 <ratkocerialnit@gmail.com>
2026-07-06 16:06:35 +0100

    First analytical query: QPR wins home and away

--- query.py
+++ query.py
@@ -2,7 +2,15 @@ import sqlite3
 
 con = sqlite3.connect("championship.db")
 sql = """
--- your query here
+select m.match_date,
+ht.team_name AS home,
+m.home_goals || '-' || m.away_goals AS score,
+at.team_name AS away
+FROM matches m 
+JOIN teams ht ON m.home_team_id = ht.team_id
+JOIN teams at ON m.away_team_id = at.team_id
+WHERE (ht.team_name = 'QPR' and m.result = 'H' )
+OR (at.team_name = 'QPR' and m.result = 'A');
 """
 for row in con.execute(sql):
     print(row)
